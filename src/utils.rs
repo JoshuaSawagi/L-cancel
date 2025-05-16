6f0f29c977e8fe8d141597450bdc04eec8fc979b
Sawagi <132923266+JoshuaSawagi@users.noreply.github.com>
2025-05-16 08:12:07 -0500

    utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,9 @@
 use smash::app;
 use smash::lib::lua_const::*;
+use smash::app::utility;
 
-pub fn get_category(boma: &mut app::BattleObjectModuleAccessor) -> i32{
-    return (boma.vtable >> 28) as u8 as i32;
+pub fn get_category(boma: &mut app::BattleObjectModuleAccessor) -> i32 {
+    utility::get_category(boma) as i32
 }
 
 pub unsafe fn get_player_number(boma: &mut app::BattleObjectModuleAccessor) -> usize{
